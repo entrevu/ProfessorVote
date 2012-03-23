5133b0e8486ba8fe859fd686d775c15791e02074
Bensmcc@gmail.com <bensmcc@gmail.com>
2012-03-22 21:05:44 -0400

    Some how I had messed up the new user thing. Fixed it now.

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -15,11 +15,11 @@ class User_model extends CI_Model {
 	{
 		
 		$new_user_insert_data = array(
-			'first_name' => $this->input->post('first_name'),
-			'last_name' => $this->input->post('last_name'),
-			'email_address' => $this->input->post('email_address'),			
-			'username' => $this->input->post('username'),
-			'password' => md5($this->input->post('password'))						
+			'first_name' => $first,
+			'last_name' => $last,
+			'email_address' => $email,	
+			'username' => $username,
+			'password' => md5($password)						
 		);
 		
 		$insert = $this->db->insert('User', $new_user_insert_data);
