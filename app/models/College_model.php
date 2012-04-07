a33c2a3425c7e07c2e9b8e190712a87fabc6ccc7
John Ulman <johnulman@gmail.com>
2012-04-07 02:58:20 -0400

    Create College Page

--- app/models/College_model.php
+++ app/models/College_model.php
@@ -22,5 +22,23 @@ class College_model extends CI_Model {
 			return FALSE;
         }
 	}
+	
+	function collegeStateExists($college, $state){ // function to see if there is a college of a certain name that already exists in a state.
+		$q = $this -> db -> query("SELECT * FROM `COLLEGE` Where Name = '".$college."' AND State = '".$state."'");
+        if ($q -> num_rows() > 0) {
+            return TRUE;
+        }
+		else{
+		return FALSE;
+		}
+	}
+	
+	function create_college($college_name, $state, $city) {
+
+		$new_college_insert_data = array('name' => $college_name, 'city' => $city, 'state' => $state);
+
+		$insert = $this -> db -> insert('College', $new_college_insert_data);
+		return $insert;
+	}
 
 }
---
+++ app/views/CollegeConfirm.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<?php    if (isset($isConfirm)) {  ?>
+					<div class="alert alert-success"><p> College Successfully Added! </p></div>
+					
+					<?php } ?>
+					
+					ADD COLLEGE VIEW AREA
+					
+					<h1>College: <?php echo $collegeName ?></h1>
+				
+</div>
