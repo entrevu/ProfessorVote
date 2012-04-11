b492f050f6f0151a5eb69fabdb2c680e62b770ae
Andres Castro <acastr7@gmail.com>
2012-04-11 17:43:06 -0400

    Changed the name of the test pages I was using.

--- app/controllers/TestPulse.php
+++ app/controllers/TestPulse.php
@@ -1,11 +1,11 @@
 <?php
 include('CoursePulse/Pulse.vote.class.php');
-class CollegeSearch extends CI_Controller {
+class TestPulse extends CI_Controller {
 
 	function index() {
 		$this -> load -> model('College_model');
 		$data['records'] = $this -> College_model -> getAll();
-		$data['main_content'] = 'CollegeSearchPage';
+		$data['main_content'] = 'TestPulsePage';
 		$this -> load -> view('includes/template', $data);
 	}
 
